aa6d6056d307bdc33293e273984a15c9622e2202
V_shiftz <boh.jieqi@dhs.sg>
2016-07-30 12:56:17 +0800

    Added DadJokes

---
+++ public/index.js
@@ -0,0 +1,11 @@
+$(function() {
+    $("#getJoke").click(
+        function() {
+            $.get("/jokes",function(data){
+                // TODO: change HTML instead of alert
+                $("#setup").html(data.setup);    
+                $("#punchline").html(data.punchline);
+            },"json")
+        }
+    );
+});
